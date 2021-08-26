0304ac135a17bd4fc212163cf2edc490ce1ba867
Richard Stanton <stanton1191@gmail.com>
2021-08-26 23:32:17 +0100

    adding humidity forecast

---
+++ assets/js/humidity-forecast/main.js
@@ -0,0 +1,173 @@
+function submitPostcode() {
+    postCode = getPostcodeForm()
+    postCode = formatPostcode(postCode)
+    document.getElementById("postcodeInput").value = postCode
+  
+    getForecast(postCode, adjustPlot)
+  }
+  
+  function getPostcodeForm() {
+    var postCode = document.getElementById("postcodeInput").value;
+    if (postCode == "") {
+      postCode = document.getElementById("postcodeInput").getAttribute("placeholder");
+    }
+    return postCode
+  }
+  
+  function formatPostcode(postCode) {
+    // limit to first part of the postcode
+    var spacePos = postCode.search(' ')
+    if (spacePos > -1) {
+      postCode = postCode.substring(0, spacePos)
+    }
+    return postCode
+  }
+  
+  function getForecast(postcode, plottingFcn, source = 'bbc') {
+    if (source == 'bbc') {
+      return getBBCForecast(postcode, plottingFcn)
+    }
+  }
+  
+  function getBBCForecast(postcode, plottingFcn) {
+    var url = "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/" + postcode;
+  
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then(data => processForcast(data, standardiseBBCWeather, plottingFcn))
+      .catch(err => { throw err });
+  
+    // d3.json(url, data => processForcast(data, standardiseBBCWeather, plottingFcn));
+  }
+  
+  function processForcast(data, standardiseFcn, plottingFcn) {
+    standardisedData = standardiseFcn(data)
+    processedData = addInsideHumidity(standardisedData)
+  
+    traces = createTraces(processedData)
+    plottingFcn(traces)
+  }
+  
+  function standardiseBBCWeather(data) {
+    return processBBCForcasts(data['forecasts'])
+  }
+  
+  function processBBCForcasts(forecasts) {
+    var list = []
+    for (var key in forecasts) {
+      result = processBBCReports(forecasts[key]["detailed"]["reports"])
+      list = list.concat(result)
+    }
+    return list
+  }
+  
+  function processBBCReports(reports) {
+    reportOutput = []
+    reports.forEach(function (report) {
+      var date = new Date(report['localDate'])
+      date.setHours(Number(report['timeslot'].substring(0, 2)))
+  
+      reportOutput.push({ date: date, outside_humidity: report['humidity'], outside_temp: report['temperatureC'] })
+    })
+    return reportOutput
+  }
+  
+  function saturatePressure(temp) {
+    var pressure = 6.122 * Math.exp(17.62 * temp / (243.12 + temp))
+    return pressure
+  }
+  
+  function getInsideHumidity(outside_temp, outside_humidity, inside_temp) {
+    inside_temp = 21
+    return (
+      (inside_temp + 273)
+      * outside_humidity
+      * saturatePressure(outside_temp)
+      / ((outside_temp + 273) * saturatePressure(inside_temp))
+    )
+  }
+  
+  function addInsideHumidity(data) {
+    data.forEach(function (row) {
+      row['inside_humidity'] = getInsideHumidity(row['outside_temp'], row['outside_humidity'], row['inside_temp'])
+    })
+    return data
+  }
+  
+  function createTraces(data) {
+    dateList = listFromDicts(data, 'date')
+  
+    traceInHum = createTrace(dateList, listFromDicts(data, 'inside_humidity'), 'Inside humidity')
+    traceOutHum = createTrace(dateList, listFromDicts(data, 'outside_humidity'), 'Outside humidity')
+    traceOutTemp = createTrace(dateList, listFromDicts(data, 'outside_temp'), 'Outside temperature', 'legendonly')
+  
+    traces = [traceInHum, traceOutHum, traceOutTemp]
+    return traces
+  }
+  
+  function listFromDicts(data, key) {
+    var list = []
+    data.forEach(function (element) {
+      list.push(element[key])
+    })
+    return list
+  }
+  
+  function createTrace(x, y, name, visible = true) {
+    var trace = {
+      type: "scatter",
+      mode: "lines",
+      name: name,
+      x: x,
+      y: y,
+      visible: visible,
+    }
+    return trace
+  }
+  
+  function createPlot(traces) {
+    var xrangemin = new Date;
+    var xrangemax = new Date(xrangemin.getTime()).setDate(xrangemin.getDate() + 3);
+    var layout = {
+      title: plotTitle,
+      height: 800,
+      xaxis: {
+        range: [xrangemin, xrangemax],
+        rangeslider: {}
+      },
+    };
+    updatePlotData(traces)
+    Plotly.newPlot(plotDiv, plotData, layout);
+  }
+  
+  function updatePlotData(traces) {
+    plotData.length = 0
+    traces.forEach(trace => {
+      plotData.push(trace)
+    });
+  }
+  
+  function adjustPlot(traces) {
+    updatePlotData(traces)
+    Plotly.redraw(plotDiv);
+  }
+  
+  // 
+  var form = document.getElementById("postCodeForm");
+  function handleForm(event) {
+    event.preventDefault();
+    submitPostcode();
+  }
+  form.addEventListener('submit', handleForm);
+  
+  const plotDiv = 'plotDiv'
+  const plotTitle = ''
+  var plotData = []
+  
+  getForecast('SW1A', createPlot)
+  
---
+++ pages/humidity-forecast.md
@@ -0,0 +1,31 @@
+---
+layout: page
+title: Humidity Forecast
+feature-img: "assets/img/pexels/rain.jpeg"
+# tags: [About, Archive]
+bootstrap: true
+comments: true
+---
+<script src='https://cdn.plot.ly/plotly-2.3.1.min.js'></script>
+
+<div class="container-xl">
+    <form id="postCodeForm">
+        <div class="row row-cols-lg-auto g-3 justify-content-md-center">
+            <div class="col">
+                <label for="postcodeInput" class="col-form-label col-form-label-lg">UK post code</label>
+                <input type="text" class="form-control-lg" id="postcodeInput" placeholder="SW1A 2JR">
+            </div>
+            <div class="col">
+                <button type="submit" class="btn btn-primary btn-lg" onClick="submitPostcode()">Submit</button>
+            </div>
+        </div>
+    </form>
+    <div class="row">
+        <div class="col">
+            <div id='plotDiv'>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="/assets/js/humidity-forecast/main.js" type="text/javascript" charset="utf-8"></script>
