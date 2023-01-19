2f4f62781dbdf7cbdbb7db6b2e2a02ade575a863
stanton119 <stanton1191@gmail.com>
2023-01-19 22:12:23 +0000

    config and docker changes

--- local_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ".bundles_cache"
-docker run --rm --name jekyllsite \
-  -v "$PWD:/srv/jekyll" \
-  -e BUNDLE_PATH="/srv/jekyll/.bundles_cache" \
-  -p 4000:4000 \
-  jekyll/builder:3.8 \
-  bash -c "gem install bundler && bundle install && bundle exec jekyll serve --host 0.0.0.0 --verbose --config _config.yml,_config_dev.yml"
